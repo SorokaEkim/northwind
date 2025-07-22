688589cc695c4000f79ea31a1bdc521e491ca29e
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2025-07-22 18:32:12 +0300

    task2.sql

--- homework.sql
+++ homework.sql
@@ -20,6 +20,37 @@ SELECT COUNT(DISTINCT customer_id) FROM customers; -- Уникальные за�
 -- Посчитать кол-во уникальных стран в которых "зарегистрированы" заказчики
 SELECT COUNT (DISTINCT country) FROM customers
 
+-- Выбрать все заказы из стран France, Austria, Spain.
+SELECT *
+FROM orders
+WHERE ship_country IN ('France', 'Austria', 'Spain')
+
+-- Выбрать все заказы, отсортировать по required_date (по убыванию) и отсортировать по дате отгрузке (по возрастанию).
+SELECT *
+FROM orders
+ORDER BY required_date DESC, shipped_date ASC
+
+-- Выбрать минимальную цену среди тех продуктов, которых в продаже более 30 единиц.
+SELECT MIN(unit_price)
+FROM products
+WHERE units_in_stock > 30
+
+-- Выбрать максимальную цену среди тех продуктов, которых в продаже более 30 единиц.
+SELECT MAX(unit_price)
+FROM products
+WHERE units_in_stock > 30
+
+-- Найти среднее значение дней уходящих на доставку с даты формирования заказа в USA
+SELECT AVG(shipped_date - order_date)
+FROM orders
+WHERE ship_country = 'USA'
+
+-- Найти сумму, на которую имеется товаров (кол-во * цену) причём таких, которые планируется продавать и в будущем (см. на поле discontinued).
+SELECT SUM(units_in_stock * unit_price)
+FROM products
+WHERE discontinued = '0'
+
+
 
 
 
