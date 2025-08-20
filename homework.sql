de070322a4d10c7d03c8e3fb7df906a618d7c13d
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2025-08-20 17:53:06 +0300

    Update homework.sql

--- homework.sql
+++ homework.sql
@@ -115,6 +115,44 @@ EXCEPT
 SELECT country
 FROM employees
 
+-- Создать таблицу teacher с полями teacher_id serial, first_name varchar, last_name varchar, birthday date, phone varchar, title varchar
+CREATE TABLE teacher (
+	teacher_id serial,
+	first_name varchar,
+	last_name varchar,
+	birthday date,
+	phone varchar,
+	title varchar
+)
+
+-- Добавить в таблицу после создания колонку middle_name varchar
+ALTER TABLE teacher 
+DROP COLUMN middle_name;
+
+-- Переименовать колонку birthday в birth_date
+ALTER TABLE teacher 
+RENAME COLUMN birthday TO birth_date;
+
+-- Изменить тип данных колонки phone на varchar(32)
+ALTER TABLE teacher 
+ALTER COLUMN phone TYPE varchar(32);
+
+-- Создать таблицу exam с полями exam_id serial, exam_name varchar(256), exam_date date
+CREATE TABLE exam (
+	exam_id serial,
+	exam_name varchar(256),
+	exam_date date
+)
+
+-- Вставить три любых записи с автогенерацией идентификатора
+INSERT INTO exam (exam_name, exam_date) VALUES
+('Математика - базовый уровень', '2024-03-15'),
+('Физика - профильный уровень', '2024-03-17'),
+('Информатика - олимпиада', '2024-03-20');
+
+-- Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE exam RESTART IDENTITY;
+
 
 
 
